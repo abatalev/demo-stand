a13121429790436c7e624d63ee1abc1a97e66d9e
andrey <andrey.batalev@gmail.com>
2021-11-07 13:52:52 +0300

    docker-compose.yaml formatting

--- mk.sh
+++ mk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_ACTIONS=1
+BUILD_ACTIONS=0
 BUILD_CONFIGURER=0
 BUILD_BALANCER=0
 BUILD_INITDB=0
@@ -10,22 +10,22 @@ cd "${CDIR}/build"
 
 if [ "${BUILD_ACTIONS}" == "1" ]; then
     echo "### build actions"
-    docker build -t abatalev/actions:2021-09-26 -f Dockerfile.actions .
+    docker build -t abatalev/actions:2021-10-28 -f Dockerfile.actions .
     if [  $? -ne 0  ]; then exit 1; fi
 fi
 if [ "${BUILD_CONFIGURER}" == "1" ]; then
     echo "### build configurer"
-    docker build -t abatalev/configurer:2021-09-26 -f Dockerfile.configurer .
+    docker build -t abatalev/configurer:2021-10-28 -f Dockerfile.configurer .
     if [  $? -ne 0  ]; then exit 1; fi;
 fi
 if [ "${BUILD_BALANCER}" == "1" ]; then
     echo "### build balancer"
-    docker build -t abatalev/balancer:2021-09-26 -f Dockerfile.balancer .
+    docker build -t abatalev/balancer:2021-10-28 -f Dockerfile.balancer .
     if [  $? -ne 0  ]; then exit 1; fi;
 fi
 if [ "${BUILD_INITDB}" == "1" ]; then
     echo "### build initdb"
-    docker build -t abatalev/initdb:2021-09-26 -f Dockerfile.initdb .
+    docker build -t abatalev/initdb:2021-10-28 -f Dockerfile.initdb .
     if [  $? -ne 0  ]; then exit 1; fi;
 fi
 
