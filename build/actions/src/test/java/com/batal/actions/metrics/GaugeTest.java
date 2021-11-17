cbb5403df05f3d46684a9a06cbdbd9f57b76b1c4
andrey <andrey.batalev@gmail.com>
2021-11-17 18:47:57 +0300

    GaugeTest: refactoring

--- build/actions/src/test/java/com/batal/actions/metrics/GaugeTest.java
+++ build/actions/src/test/java/com/batal/actions/metrics/GaugeTest.java
@@ -9,66 +9,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GaugeTest {
 
     @Test
-    public void checkNano(){
-        GaugeSet.Gauge gauge = new GaugeSet.Gauge( 5);
+    public void checkNano() {
+        GaugeSet.Gauge gauge = new GaugeSet.Gauge(5);
         LocalDateTime time = LocalDateTime.of(2021, 01, 01, 01, 01, 01);
         gauge.incWithTime(time);
-        LocalDateTime time1 = time.plusNanos(100000);
-        gauge.incWithTime(time1);
-        LocalDateTime time2 = time.plusSeconds(1);
-        gauge.incWithTime(time2);
-        assertEquals(3, gauge.getWithTime(time2));
+        gauge.incWithTime(time.plusNanos(100000));
+        incAndAssert(gauge, time.plusSeconds(1), 3);
     }
 
     @Test
     public void checkMyGaugeInOneTime() {
-        GaugeSet.Gauge gauge = new GaugeSet.Gauge( 5);
+        GaugeSet.Gauge gauge = new GaugeSet.Gauge(5);
         LocalDateTime time = LocalDateTime.now();
-        gauge.incWithTime(time);
-        assertEquals(1, gauge.getWithTime(time));
-        gauge.incWithTime(time);
-        assertEquals(2, gauge.getWithTime(time));
-        gauge.incWithTime(time);
-        assertEquals(3, gauge.getWithTime(time));
-        gauge.incWithTime(time);
-        assertEquals(4, gauge.getWithTime(time));
+        incAndAssert(gauge, time, 1);
+        incAndAssert(gauge, time, 2);
+        incAndAssert(gauge, time, 3);
+        incAndAssert(gauge, time, 4);
     }
 
     @Test
-    public void checkLongPeriod(){
-        GaugeSet.Gauge gauge = new GaugeSet.Gauge( 5);
+    public void checkLongPeriod() {
+        GaugeSet.Gauge gauge = new GaugeSet.Gauge(5);
         LocalDateTime time = LocalDateTime.of(2021, 01, 01, 01, 01, 01);
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        time = time.plusSeconds(6);
-        gauge.incWithTime(time);
-        assertEquals(1, gauge.getWithTime(time));
+        time = step(gauge, time, 1, 1);
+        step(gauge, time, 1, 6);
     }
 
     @Test
     public void checkMyGaugeIn5Seconds() {
-        GaugeSet.Gauge gauge = new GaugeSet.Gauge( 5);
+        GaugeSet.Gauge gauge = new GaugeSet.Gauge(5);
         LocalDateTime time = LocalDateTime.of(2021, 01, 01, 01, 01, 01);
-        time = time.plusSeconds(1);
+        time = step(gauge, time, 1, 1);
+        time = step(gauge, time, 2, 1);
+        time = step(gauge, time, 3, 1);
+        time = step(gauge, time, 4, 1);
+        time = step(gauge, time, 5, 1);
+        time = step(gauge, time, 5, 1);
+        step(gauge, time, 5, 1);
+    }
+
+    private LocalDateTime step(GaugeSet.Gauge gauge, LocalDateTime time, int expected, int cnt) {
+        time = time.plusSeconds(cnt);
+        incAndAssert(gauge, time, expected);
+        return time;
+    }
+
+    private void incAndAssert(GaugeSet.Gauge gauge, LocalDateTime time, int expected) {
         gauge.incWithTime(time);
-        assertEquals(1, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(2, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(3, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(4, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(5, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(5, gauge.getWithTime(time));
-        time = time.plusSeconds(1);
-        gauge.incWithTime(time);
-        assertEquals(5, gauge.getWithTime(time));
+        assertEquals(expected, gauge.getWithTime(time));
     }
 }
