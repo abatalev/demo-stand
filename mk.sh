654a7bdc0cb0d37e914f9d74e399cb259de7651b
andrey <andrey.batalev@gmail.com>
2021-12-22 09:04:07 +0300

    use yq

--- mk.sh
+++ mk.sh
@@ -4,6 +4,12 @@ B_CONFIGURER=1
 B_BALANCER=1
 B_INITDB=1
 
+I_GROUP="abatalev"
+I_ACTIONS="${I_GROUP}/actions"
+I_BALANCER="${I_GROUP}/balancer"
+I_CONFIGURER="${I_GROUP}/configurer"
+I_INITDB="${I_GROUP}/initdb"
+
 V_ACTIONS="2021-10-28"
 V_CONFIGURER="2021-10-28"
 V_BALANCER="2021-10-28"
@@ -19,38 +25,43 @@ if [ -f "prj2hash" ]; then
     V_BALANCER=$(./prj2hash -short build/balancer)
     V_INITDB=$(./prj2hash -short build/initdb)
 
-    B_ACTIONS=$(image_exist abatalev/actions $V_ACTIONS)
-    B_CONFIGURER=$(image_exist abatalev/configurer $V_CONFIGURER)
-    B_BALANCER=$(image_exist abatalev/balancer $V_BALANCER)
-    B_INITDB=$(image_exist abatalev/initdb $V_INITDB)
+    B_ACTIONS=$(image_exist $I_ACTIONS $V_ACTIONS)
+    B_CONFIGURER=$(image_exist $I_CONFIGURER $V_CONFIGURER)
+    B_BALANCER=$(image_exist $I_BALANCER $V_BALANCER)
+    B_INITDB=$(image_exist $I_INITDB $V_INITDB)
 fi
+
+IV_ACTIONS=$I_ACTIONS:$V_ACTIONS
+IV_BALANCER=$I_BALANCER:$V_BALANCER
+IV_CONFIGURER=$I_CONFIGURER:$V_CONFIGURER
+IV_INITDB=$I_INITDB:$V_INITDB
 
 CDIR=$(pwd)
 cd "${CDIR}/build"
 if [ ${B_ACTIONS} == 0 ]; then
     echo "### build actions"
-    docker build -t abatalev/actions:$V_ACTIONS -f Dockerfile.actions .
+    docker build -t $IV_ACTIONS -f Dockerfile.actions .
     if [  $? -ne 0  ]; then exit 1; fi
 else 
    echo "### build actions skipped.    $V_ACTIONS"    
 fi
 if [ ${B_CONFIGURER} == 0 ]; then
     echo "### build configurer"
-    docker build -t abatalev/configurer:$V_CONFIGURER -f Dockerfile.configurer .
+    docker build -t $IV_CONFIGURER -f Dockerfile.configurer .
     if [  $? -ne 0  ]; then exit 1; fi;
 else 
    echo "### build configurer skipped. $V_CONFIGURER"    
 fi
 if [ ${B_BALANCER} == 0 ]; then
     echo "### build balancer"
-    docker build -t abatalev/balancer:$V_BALANCER -f Dockerfile.balancer .
+    docker build -t $IV_BALANCER -f Dockerfile.balancer .
     if [  $? -ne 0  ]; then exit 1; fi;
 else 
    echo "### build balancer skipped.   $V_BALANCER"    
 fi
 if [ ${B_INITDB} == 0 ]; then
     echo "### build initdb"
-    docker build -t abatalev/initdb:$V_INITDB -f Dockerfile.initdb .
+    docker build -t $IV_INITDB -f Dockerfile.initdb .
     if [  $? -ne 0  ]; then exit 1; fi;
 else 
    echo "### build initdb skipped.     $V_INITDB"    
@@ -59,10 +70,10 @@ fi
 cd "${CDIR}"
 echo "### create docker-compose"
 cp docker-compose.tmpl docker-compose.yaml
-sed -i "" "s/XX_ACTIONS_XX/${V_ACTIONS}/g" docker-compose.yaml
-sed -i "" "s/XX_CONFIGURER_XX/${V_CONFIGURER}/g" docker-compose.yaml
-sed -i "" "s/XX_BALANCER_XX/${V_BALANCER}/g" docker-compose.yaml
-sed -i "" "s/XX_INITDB_XX/${V_INITDB}/g" docker-compose.yaml
+IMAGE_VERSION=$IV_ACTIONS yq e -i '.services.actions.image = strenv(IMAGE_VERSION)' docker-compose.yaml
+IMAGE_VERSION=$IV_BALANCER yq e -i '.services.balancer.image = strenv(IMAGE_VERSION)' docker-compose.yaml
+IMAGE_VERSION=$IV_CONFIGURER yq e -i '.services.configurer.image = strenv(IMAGE_VERSION)' docker-compose.yaml
+IMAGE_VERSION=$IV_INITDB yq e -i '.services.init.image = strenv(IMAGE_VERSION)' docker-compose.yaml
 
 echo "### launch docker-compose"
 docker-compose up # --scale actions=2
